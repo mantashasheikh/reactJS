06a20f2ebdfd884fe4e662d157165ad99356d211
mantashasheikh <mantashasheikh95844@gmail.com>
2026-03-24 16:18:30 +0530

    task1

--- my-react-app/src/task1.jsx
+++ my-react-app/src/task1.jsx
@@ -2,12 +2,17 @@ function Task1() {
     return (
         <>
         <div>
-            <h1>Butterfly</h1>
-            <p>The butterfly is a beautiful insect with colorful wings.</p>
-            <button>Button</button>
+            <div>
+               <h1>Butterfly</h1>
+               <p>The butterfly is a beautiful insect with colorful wings.</p>
+                <button>Button</button>
+            </div>
+            
+        
+            <img src="" alt="" />
+
         </div>
         
-        <img src="" alt="" />
 
         </>
     )
