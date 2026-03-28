ca54e0eb08f3745be3f6842c8d693035e8546fd3
mantashasheikh <mantashasheikh95844@gmail.com>
2026-03-28 18:11:14 +0530

    react work

--- rounting/src/App.jsx
+++ rounting/src/App.jsx
@@ -1,11 +1,21 @@
-import {Route , Routes} from 'react-router-dom'
+import {Route , Routes , Link} from 'react-router-dom'
 import Home from './home'
+import About from './about'
+import Contact from './contact'
+import Services from './services'
 function App() {
   return (
     <>
-      <h1>this is app page</h1>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/services">Services</Link>
+    
       <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route index  element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
       </Routes>
     </>
     
---
+++ rounting/src/about.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <>
+      <h1>About Us</h1>
+    </>
+    
+      
+      
+    
+  )
+}
+
+export default About
